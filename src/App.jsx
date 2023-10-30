8d7ac30717ea3577dda9cf423e5aaa491ccb9f8d
Aduye <NightBirdie@users.noreply.github.com>
2023-10-30 21:58:22 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import "./App.css";
+
+const images = [
+  "https://picsum.photos/seed/img6/600/400",
+  "https://picsum.photos/600/400?image=19",
+  "https://picsum.photos/seed/img28/600/400",
+  "https://picsum.photos/seed/img21/600/400",
+  "https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
+];
+
+export default function App() {
+  const [current, setCurrent] = useState(0);
+
+  function nextSlide() {
+    setCurrent(current === images.length - 1 ? 0 : current + 1);
+  }
+
+  function prevSlide() {
+    setCurrent(current === 0 ? images.length - 1 : current - 1);
+  }
+
+  return (
+    <div>
+      <h2>Day 1 Image Carousel</h2>
+      <div className="slider">
+        <div className="left-arrow" onClick={prevSlide}>
+          ⬅
+        </div>
+        <div className="right-arrow" onClick={nextSlide}>
+          ⮕
+        </div>
+        {images.map(
+          (image, index) =>
+            current === index && (
+              <div key={image} className="slide">
+                <img src={image} alt="images" />
+              </div>
+            )
+        )}
+      </div>
+    </div>
+  );
+}
